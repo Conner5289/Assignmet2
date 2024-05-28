5527097db92d913c70ba3f4054257290bf2c68c7
Conner <conneryule@gmail.com>
2024-05-28 13:24:17 -0400

    Made it so that the user could filp coin how many times they want

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import random
 
+how_many_times = input('How many times do you want to flip the coin?')
+how_many_times_int = int(how_many_times)
 
-toss = random.randint(1, 2)
+while how_many_times_int != 0:
+    toss = random.randint(1, 2)
 
-if toss == 1:
-    coin = 'head'
-else:
-    coin = 'tails'
+    if toss == 1:
+        coin = 'head'
+    else:
+        coin = 'tails'
 
-
-print(coin)
+    print(coin)
+    how_many_times_int -= 1
