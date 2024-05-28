92b690bf8662075225651bea263ceb7128b53116
Conner <conneryule@gmail.com>
2024-05-28 13:46:29 -0400

    Added a counter for how many heads and tails ther was

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import random
 
-how_many_times = input('How many times do you want to flip the coin?')
-how_many_times_int = int(how_many_times)
+head = 0
+tails = 0
 
-while how_many_times_int != 0:
+how_many_flips = input('How many times do you want to flip the coin?')
+how_many_flips_int = int(how_many_flips)
+
+while how_many_flips_int != 0:
     toss = random.randint(1, 2)
 
     if toss == 1:
         coin = 'head'
+        head += 1
     else:
         coin = 'tails'
+        tails += 1
 
     print(coin)
-    how_many_times_int -= 1
+    how_many_flips_int -= 1
+
+print(f'There was {head} heads')
+print(f'There was {tails} tails')
