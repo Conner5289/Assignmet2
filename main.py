f81302dcbbd8e587ae50b659b1f1414cf1f4035d
Conner <conneryule@gmail.com>
2024-05-29 23:27:55 -0400

    Add working bar graph to show the amount of heads or tails

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import random
 class coin_flip:
     def flip():
         head = 0
-        tails = 0
+        tail = 0
 
-        how_many_flips = input('How many times do you want to flip the coin? ')
+        how_many_flips = input('How many times do you want to flip the coin?')
         how_many_flips_int = int(how_many_flips)
 
         while how_many_flips_int != 0:
             toss = random.randint(1, 2)
 
             if toss == 1:
-                coin = 'head'
+                # coin = 'head'
                 head += 1
             else:
-                coin = 'tails'
-                tails += 1
+                # coin = 'tails'
+                tail += 1
 
-            print(coin)
+            # print(coin)
             how_many_flips_int -= 1
+        total = head + tail
 
-        print(f'There was {head} heads')
-        print(f'There was {tails} tails')
+        return [head, tail, total]
